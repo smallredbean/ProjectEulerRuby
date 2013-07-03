a0e3fadb3fa891df00fe46b37be6fa9c0e7a582b
Beany H <huangyl@gmail.com>
2013-07-03 11:25:00 -0400

    modified:   Prime.rb Add Prime.within Change the implementation of Prime.upto to use Prime.within

--- Prime.rb
+++ Prime.rb
@@ -1,13 +1,32 @@
 class Prime
-	def Prime.upto max
-		table = []
-		n = 2
-		while n<=max
-			yield n
-
-			table[n] = false
-			(n*n).step(max, n) {|num| table[num] = false}
-			n += 1 until table[n]!=false
+	def Prime.within from, to, priorPrimes=[]
+		if from>2 and priorPrimes==[]
+			priorPrimes = Prime.within 2, from-1
 		end
+
+		# update the table with previous prime
+		table = Hash.new(true)
+		priorPrimes.each {|prime|
+			newFrom = [(from/prime.to_f).ceil*prime, prime*prime].max
+			newFrom.step(to, prime) {|num| table[num] = false}
+		}
+
+		# generate primes in [from, to]
+		primes = []
+		prime = from
+		while true
+			prime += 1 until table[prime]==true
+			break if prime>to
+
+			primes.push prime
+			table[prime] = false
+			(prime*prime).step(to, prime) {|num| table[num] = false}
+		end
+
+		primes
+	end
+
+	def Prime.upto max
+		(Prime.within 2, max).each {|prime| yield prime}
 	end
 end
