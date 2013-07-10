ac9f1cabd1370662a327191121429d653badf8d7
Beany H <huangyl@gmail.com>
2013-07-10 11:02:21 -0400

    renamed:    prob_32_2.rb -> prob_32.rb rename

--- prob_32.rb
+++ prob_32.rb
@@ -11,4 +11,27 @@
 # Hence, we know that D(a)+D(b) must be 5, which limits the searching range
 # That is, [D(a), D(b)] is [1, 4] or [2, 3]
 
+require "MyInteger"
 
+def search dA, dB
+	(10**(dA-1)...10**dA).each {|a|
+		next if a.zeroInDigits or a.hasDigitInDuplicate
+		(10**(dB-1)...10**dB).each {|b|
+			if b.zeroInDigits or b.hasDigitInDuplicate or a.hasSameDigitAs b
+				next
+			else
+				c = a*b
+				break if c.to_s.length>4
+				next if c.zeroInDigits or c.hasDigitInDuplicate or a.hasSameDigitAs c or b.hasSameDigitAs c
+				@products[c] = true
+			end
+		}
+	}
+end
+
+@products = Hash.new
+search 1, 4
+search 2, 3
+sum = 0
+@products.each {|product, valueTF| sum += product}
+puts sum
